84f61ce61b0e5bd26c7ae512d9d34f3077f1ac45
hsha02 <hsha02@hsha02-macbook.local>
2023-04-30 17:49:15 +0900

    post repository test code

---
+++ src/test/java/com/kombucha/kombucha/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.kombucha.kombucha.domain.posts;
+
+import com.kombucha.domain.posts.Posts;
+import com.kombucha.domain.posts.PostsRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글_저장_불러온다() {
+        String title = "게시글 제목";
+        String content = "게시글 내용";
+        String author = "관리자";
+
+        postsRepository.save(Posts.builder().title(title).content(content).author(author).build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+        assertThat(posts.getAuthor()).isEqualTo(author);
+    }
+}
