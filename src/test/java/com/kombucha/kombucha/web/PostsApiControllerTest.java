f59c076349706ab2cfff65382b0f86402119774c
hsha02 <hsha02@hsha02-macbook.local>
2023-04-30 17:56:51 +0900

    필요없는 코드 삭제

--- src/test/java/com/kombucha/kombucha/web/PostsApiControllerTest.java
+++ src/test/java/com/kombucha/kombucha/web/PostsApiControllerTest.java
@@ -15,14 +15,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
