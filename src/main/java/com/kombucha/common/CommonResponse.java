ea76ddba91157695a4f2c12d4c76bfb05eba5d94
hsha02 <hsha02@hsha02-macbook.local>
2023-05-21 20:09:59 +0900

    fix: api response data가 Array 형태로 오는 오류 수정

--- src/main/java/com/kombucha/common/CommonResponse.java
+++ src/main/java/com/kombucha/common/CommonResponse.java
@@ -1,6 +1,5 @@
 package com.kombucha.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
@@ -17,7 +16,17 @@ public class CommonResponse {
     private final String message;
     private Object data;
 
-    public static ResponseEntity<CommonResponse> toResponseEntity(HttpStatus statusCode, Object... data) {
+    public static ResponseEntity<CommonResponse> toResponseEntity(HttpStatus statusCode) {
+        return ResponseEntity
+                .status(statusCode.value())
+                .body(CommonResponse.builder()
+                        .status(statusCode.value())
+                        .code(statusCode.value())
+                        .message(statusCode.getReasonPhrase())
+                        .build());
+    }
+
+    public static ResponseEntity<CommonResponse> toResponseEntity(HttpStatus statusCode, Object data) {
         return ResponseEntity
                 .status(statusCode.value())
                 .body(CommonResponse.builder()
@@ -27,7 +36,16 @@ public class CommonResponse {
                         .data(data)
                         .build());
     }
-    public static ResponseEntity<CommonResponse> toResponseEntity(StatusCode statusCode, Object... data) {
+    public static ResponseEntity<CommonResponse> toResponseEntity(StatusCode statusCode) {
+        return ResponseEntity
+                .status(statusCode.getStatus().value())
+                .body(CommonResponse.builder()
+                        .status(statusCode.getStatus().value())
+                        .code(statusCode.getStatus().value())
+                        .message(statusCode.getMessage())
+                        .build());
+    }
+    public static ResponseEntity<CommonResponse> toResponseEntity(StatusCode statusCode, Object data) {
         return ResponseEntity
                 .status(statusCode.getStatus().value())
                 .body(CommonResponse.builder()
